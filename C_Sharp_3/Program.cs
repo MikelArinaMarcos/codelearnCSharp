0d09de2a0ac2b72fbbf64ccb3489b41473556e0e
Mikel Arina Marcos <mikel.arina.marcos@gmail.com>
2023-03-06 18:57:52 +0100

    Welcome message with name and age

---
+++ C_Sharp_3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Codelearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int age;
+
+            Console.Write("What's your name? ");
+            name = Console.ReadLine();
+
+            Console.Write("How old are you? ");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Welcome " + name + "! You are " + age + " years old.");
+            Console.WriteLine("Press any key.");
+            Console.ReadLine();
+        }
+    }
+}
